d2a3003488de574b64a69879a04d767184db1efc
dzotov <dzotovwork@yandex.ru>
2024-04-20 12:24:32 +0300

    6 lesson calc game before refactor

---
+++ bin/brain-calc.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { calcGame } from "../src/cli.js";
+
+console.log('Welcome to the Brain Games!');
+
+calcGame();
--- src/cli.js
+++ src/cli.js
@@ -5,9 +5,8 @@ export function sayHello() {
   console.log(`Hello, ${name}!`);
 }
 
-function getRandomInt() {
-  const MAX = 100;
-  return Math.floor(Math.random() * MAX);
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 export function evenGame() {
@@ -19,11 +18,51 @@ export function evenGame() {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let correctAnswerCount = 0;
   while (correctAnswerCount !== 3) {
-    number = getRandomInt();
+    number = getRandomInt(100);
     correctAnswer = number % 2 == 0 ? 'yes' : 'no';
     console.log(`Question: ${number}`);
     answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
+      console.log('Correct');
+      correctAnswerCount++;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
+Let 's try again, ${name}!`)
+      correctAnswerCount = 0
+    };
+  }
+  console.log(`Congratulations, ${name}!`)
+}
+
+export function calcGame() {
+  const name = readlineSync.question('May I have your name? ');
+  const operations = ['+', '-', '*']
+  let operation = null;
+  let correctAnswer = null;
+  let numberA = null;
+  let numberB = null;
+  let answer = null;
+  console.log(`Hello, ${name}!`);
+  console.log('What is the result of the expression?');
+  let correctAnswerCount = 0;
+  while (correctAnswerCount !== 3) {
+    numberA = getRandomInt(100);
+    numberB = getRandomInt(100);
+    operation = operations[getRandomInt(3)]
+    switch (operation) {
+    case '+':
+      correctAnswer = numberA + numberB;
+      break
+    case '-':
+      correctAnswer = numberA - numberB;
+      break
+    case '*':
+      correctAnswer = numberA * numberB;
+      break
+    }
+    console.log(`Question: ${numberA} ${operation} ${numberB}`);
+    answer = readlineSync.question('Your answer: ');
+    if (answer == correctAnswer) {
       console.log('Correct');
       correctAnswerCount++;
     } else {
